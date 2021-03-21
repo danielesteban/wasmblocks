476856e73306017a8548d3eddcea9d6903b46cad
danielesteban <danielesteban@users.noreply.github.com>
2021-03-21 16:27:01 +0000

    deploy: d969f8116ffd0acaf0ce0ed9dd23f33fec790b15

--- renderables/chunk.js
+++ renderables/chunk.js
@@ -56,8 +56,8 @@ class VoxelChunk extends Mesh {
     geometry.setAttribute('position', new InterleavedBufferAttribute(vertices, 3, 0));
     geometry.setAttribute('color', new InterleavedBufferAttribute(vertices, 3, 3));
     if (geometry.boundingSphere === null) {
-			geometry.boundingSphere = new Sphere();
-		}
+      geometry.boundingSphere = new Sphere();
+    }
     geometry.boundingSphere.set({ x: bounds[0], y: bounds[1], z: bounds[2] }, bounds[3]);
   }
 
