87b0a07c12ce768ec8468a003c0e1474984259c0
Daniel Esteban Nombela <dani@gatunes.com>
2021-03-22 18:19:43 +0100

    Simpler pako worker api

--- vendor/pako.worker.js
+++ vendor/pako.worker.js
@@ -1,6 +1,6 @@
 importScripts('https://cdn.jsdelivr.net/npm/pako@2.0.3/dist/pako.min.js');
 
-self.addEventListener('message', ({ data: { id, buffer, operation } }) => {
-  buffer = pako[operation](buffer);
-  self.postMessage({ id, buffer }, [buffer.buffer]);
+self.addEventListener('message', ({ data: { id, data, operation } }) => {
+  data = pako[operation](data);
+  self.postMessage({ id, data }, [data.buffer]);
 });
