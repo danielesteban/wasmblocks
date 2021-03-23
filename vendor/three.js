1c3186b35a8f18f596fb8b874ed13a48a5e611ac
danielesteban <danielesteban@users.noreply.github.com>
2021-03-23 04:51:04 +0000

    deploy: 0d4b89290ca33aab343cd68341e978c3e97519f7

--- vendor/three.js
+++ vendor/three.js
@@ -1,1 +1,4 @@
 export * from 'https://cdn.jsdelivr.net/npm/three@0.126.1/build/three.module.js';
+export { EffectComposer } from 'https://cdn.jsdelivr.net/npm/three@0.126.1/examples/jsm/postprocessing/EffectComposer.js';
+export { RenderPass } from 'https://cdn.jsdelivr.net/npm/three@0.126.1/examples/jsm/postprocessing/RenderPass.js';
+export { ShaderPass } from 'https://cdn.jsdelivr.net/npm/three@0.126.1/examples/jsm/postprocessing/ShaderPass.js';
